d868a64d70b1178c7e557b5ecf3ef63d844a0dbb
skarlem <lynette9543@gmail.com>
2022-07-18 16:51:08 +0700

    Delete db.sql

--- db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE todos(
-    id serial primary key,
-    description text
-);
-INSERT INTO todos(description)VALUES ('first task');
-INSERT INTO todos(description)VALUES ('second task');
-INSERT INTO todos(description)VALUES ('third task');
